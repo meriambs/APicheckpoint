a8c448e9ea7cf8a9f0cd076390a853b9ef97eac8
meriambs <bensalah.meriem0@gmail.com>
2020-12-30 02:24:54 +0100

    before css

--- api/src/UserList.js
+++ api/src/UserList.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState , useEffect } from 'react';
+import axios from 'axios'
 
-function UserList(){
-    return(
-        <div>
+ function UserList () {
+     const [posts,setPosts]= useState([])
 
-        </div>
-    )
+     useEffect(()=>{
+     axios.get('https://jsonplaceholder.typicode.com/users')
+     .then(res=>{
+         console.log(res)
+         setPosts(res.data)
+     })
+     .catch(err=>{
+         console.log(err)
+     })
+    },[])
+  
+  return (
+    <div>
+  
+  <p>
+      {
+    posts.map(post=><li key={post.id}>{post.username}</li>)
+      }
+  </p>
+     
+    </div>
+  )
 }
 export default UserList
